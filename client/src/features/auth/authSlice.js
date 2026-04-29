044d9eca40c930aa4cdb42510e8fb2d7127f06a3
Nawin chander <nawinnapzy289@gmail.com>
2026-04-29 08:59:06 +0530

    add

--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -3,14 +3,34 @@ import { createSlice } from "@reduxjs/toolkit";
 const authSlice = createSlice({
   name: "auth",
   initialState: {
-    user: {
-      id: 1,
-      name: "Nawin",
-      role: "employee"
-    }
+    token: null,
+    user: null
   },
-  reducers: {}
+  reducers: {
+    loginSuccess: (state, action) => {
+      state.token = action.payload.token;
+      state.user = action.payload.user;
+    }
+  }
 });
 
+export const { loginSuccess } = authSlice.actions;
 export default authSlice.reducer;
+
+
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const authSlice = createSlice({
+//   name: "auth",
+//   initialState: {
+//     user: {
+//       id: 1,
+//       name: "Nawin",
+//       role: "employee"
+//     }
+//   },
+//   reducers: {}
+// });
+
+// export default authSlice.reducer;
 
