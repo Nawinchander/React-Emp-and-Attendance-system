99ac38c94f97e0a35716e81f4d6143ab6343bd51
Nawin chander <nawinnapzy289@gmail.com>
2026-04-29 09:33:42 +0530

    add

---
+++ server/test/auth.test.js
@@ -0,0 +1,11 @@
+const request = require("supertest");
+const app = require("../server");
+
+test("Login route", async () => {
+  const res = await request(app)
+    .post("/api/auth/login")
+    .send({ email: "test@mail.com" });
+
+  expect(res.statusCode).toBe(200);
+});
+
