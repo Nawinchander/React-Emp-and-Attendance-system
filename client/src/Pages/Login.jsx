10d1ab4edffb18c031056e6a79e57a0197faf959
Nawin chander <nawinnapzy289@gmail.com>
2026-04-29 09:01:16 +0530

    add

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,8 +1,28 @@
+import { useDispatch } from "react-redux";
+import api from "../api/axios";
+import { loginSuccess } from "../features/auth/authSlice";
+
 export default function Login() {
-  return (
-    <div>
-      <h2>Login Page</h2>
-    </div>
-  );
+  const dispatch = useDispatch();
+
+  const login = async () => {
+    const res = await api.post("/auth/login", {
+      email: "test@mail.com"
+    });
+
+    dispatch(loginSuccess(res.data));
+  };
+
+  return <button onClick={login}>Login</button>;
 }
+
+
+
+// export default function Login() {
+//   return (
+//     <div>
+//       <h2>Login Page</h2>
+//     </div>
+//   );
+// }
 
